a9807cc58cda69c7693dca46cadc56a17d4f6707
deveshgarg-max <deveshgarg9829@gmial.com>
2021-01-05 17:44:32 +0530

    json changed and optimization

--- lib/auth/LoginScreen.dart
+++ lib/auth/LoginScreen.dart
@@ -1,17 +1,14 @@
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:lottie/lottie.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import '../Screens/HomeScreen.dart';
 import '../main.dart';
 import 'otpverificationscreen.dart';
@@ -43,8 +40,6 @@ class _LoginScreenState extends State<LoginScreen>
     super.dispose();
   }
 
-// {name: Devesh Garg, email: deveshgarg9829@gmail.com, picture: {data: {height: 126, is_silhouette: true, url: https://scontent.fjai2-1.fna.fbcdn.net/v/t1.30497-1/s200x200/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=OkCTBOF_dxEAX9oSKhO&_nc_ht=scontent.fjai2-1.fna&tp=7&oh=70e2f655256aa4b01d3ba60b54fcffc1&oe=6017B3E5, width: 200}}, id: 425013675527056}
-
   Future<void> _loginWithFacebook() async {
     try {
       AccessToken accessToken = await FacebookAuth.instance.login();
@@ -54,6 +49,9 @@ class _LoginScreenState extends State<LoginScreen>
       final userData = await FacebookAuth.instance.getUserData();
       print(userData);
       try {
+        setState(() {
+          _isFacebookLoggin = true;
+        });
         final user = await FirebaseAuth.instance
             .signInWithCredential(_facebookcredentials);
         if (user != null) {
@@ -80,6 +78,10 @@ class _LoginScreenState extends State<LoginScreen>
         print(user.user.uid); // get the user data
         print("==========================================================");
       } catch (e) {
+        setState(() {
+          _isFacebookLoggin = false;
+        });
+
         FirebaseAuthException error = e;
         Fluttertoast.showToast(msg: error.message);
       }
@@ -101,54 +103,54 @@ class _LoginScreenState extends State<LoginScreen>
   void _signInWithGoogle() async {
     final googleSignIn = GoogleSignIn();
     final firebaseAuth = FirebaseAuth.instance;
-    // try {
-    final GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    try {
+      final GoogleSignInAccount googleUser = await googleSignIn.signIn();
 
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-    setState(() {
-      _isGoogleLogging = true;
-    });
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      setState(() {
+        _isGoogleLogging = true;
+      });
 
-    final AuthCredential credential = GoogleAuthProvider.credential(
-        idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
-    final user = (await firebaseAuth.signInWithCredential(credential)).user;
-    await FirebaseDatabase.instance
-        .reference()
-        .child("User Information")
-        .child(FirebaseAuth.instance.currentUser.uid)
-        .update({
-      "userName": googleUser.displayName,
-      "emial": googleUser.email,
-      "imageURL": googleUser.photoUrl,
-      "PlanName": "fdsadf",
-    });
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+      final user = (await firebaseAuth.signInWithCredential(credential)).user;
+      await FirebaseDatabase.instance
+          .reference()
+          .child("User Information")
+          .child(FirebaseAuth.instance.currentUser.uid)
+          .update({
+        "userName": googleUser.displayName,
+        "emial": googleUser.email,
+        "imageURL": googleUser.photoUrl,
+        "PlanName": "fdsadf",
+      });
 
-    if (user != null) {
+      if (user != null) {
+        setState(() {
+          _isGoogleLogging = false;
+        });
+
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ),
+        );
+      } else {
+        setState(() {
+          _isGoogleLogging = false;
+        });
+        Fluttertoast.showToast(msg: "Something went wrong");
+      }
+    } catch (e) {
       setState(() {
         _isGoogleLogging = false;
       });
-
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => HomeScreen(),
-        ),
-      );
-    } else {
-      setState(() {
-        _isGoogleLogging = false;
-      });
+      print("------------------------");
+      print(e);
+      print("------------------------");
       Fluttertoast.showToast(msg: "Something went wrong");
     }
-    // } catch (e) {
-    //   setState(() {
-    //     _isGoogleLogging = false;
-    //   });
-    //   print("------------------------");
-    //   print(e);
-    //   print("------------------------");
-    //   Fluttertoast.showToast(msg: "Something went wrong");
-    // }
   }
 
   @override
@@ -314,29 +316,34 @@ class _LoginScreenState extends State<LoginScreen>
                                   child: Padding(
                                     padding: const EdgeInsets.symmetric(
                                         horizontal: 10),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        Container(
-                                          height: height * 0.045,
-                                          child: Image.asset(
-                                            'assets/google.png',
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: width * 0.01,
-                                        ),
-                                        Text(
-                                          "Sign in with Google",
-                                          style: GoogleFonts.roboto(
-                                            fontSize: 16,
+                                    child: _isGoogleLogging
+                                        ? SpinKitThreeBounce(
                                             color: Colors.white,
+                                            size: 20,
+                                          )
+                                        : Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              Container(
+                                                height: height * 0.045,
+                                                child: Image.asset(
+                                                  'assets/google.png',
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                width: width * 0.01,
+                                              ),
+                                              Text(
+                                                "Sign in with Google",
+                                                style: GoogleFonts.roboto(
+                                                  fontSize: 16,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                            ],
                                           ),
-                                        ),
-                                      ],
-                                    ),
                                   ),
                                 ),
                               ),
@@ -355,29 +362,34 @@ class _LoginScreenState extends State<LoginScreen>
                                   child: Padding(
                                     padding: const EdgeInsets.symmetric(
                                         horizontal: 10),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        Container(
-                                          height: height * 0.035,
-                                          child: Image.asset(
-                                            'assets/fb.png',
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: width * 0.025,
-                                        ),
-                                        Text(
-                                          "Sign in with Facebook",
-                                          style: GoogleFonts.roboto(
-                                            fontSize: 16,
+                                    child: _isFacebookLoggin
+                                        ? SpinKitThreeBounce(
                                             color: Colors.white,
+                                            size: 20,
+                                          )
+                                        : Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              Container(
+                                                height: height * 0.035,
+                                                child: Image.asset(
+                                                  'assets/fb.png',
+                                                  fit: BoxFit.cover,
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                width: width * 0.025,
+                                              ),
+                                              Text(
+                                                "Sign in with Facebook",
+                                                style: GoogleFonts.roboto(
+                                                  fontSize: 16,
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                            ],
                                           ),
-                                        ),
-                                      ],
-                                    ),
                                   ),
                                 ),
                               ),
