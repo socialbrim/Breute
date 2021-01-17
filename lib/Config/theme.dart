369c569c29d98154f8b938341aa6fc49f26cafda
Harsh Mehta <mharsh13dec@gmail.com>
2021-01-18 00:35:35 +0530

    some theme changes

--- lib/Config/theme.dart
+++ lib/Config/theme.dart
@@ -12,11 +12,11 @@ class AppThemeData extends ChangeNotifier {
   Color colorGrey = Color.fromARGB(255, 209, 210, 205);
 
   // Color colorPrimary = Color(0xff009688);
-  Color colorPrimary = HexColor('ffa199');
-  Color colorCompanion = HexColor('f73378');
+  Color colorPrimary = HexColor('3CB5FA');
+  Color colorCompanion = HexColor('F968C1');
   Color colorCompanion2 = Color(0xffffbb43);
   Color colorCompanion3 = Color(0xffb6e9d1);
-  Color colorCompanion4 = Colors.green;
+  Color colorCompanion4 = HexColor("EB2C7F");
 
   Color colorBackground;
   Color colorBackgroundGray;
